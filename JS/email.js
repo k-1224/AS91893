adf8bf68a23b9a493d1a6b2c2594d936e87e8f1b
k-1224 <st25386@ormiston.school.nz>
2026-05-18 12:12:37 +1200

    update my email.js

--- JS/email.js
+++ JS/email.js
@@ -6,14 +6,14 @@ function sendMail() {
     feedback  : document.querySelector("#feedback").value,
   };
   
-if (!firstname || !email || !subject || !feedback) {
-  return false;
-}
+  if (!templateParams.firstname || !templateParams.email || !templateParams.subject || !templateParams.feedback) {
+    return false;
+  }
 
   emailjs.send("service_7z7fes1", "template_1uwwhys", templateParams)
-    .then(() => {
-      alert("Email Sent!");
-    })
+  .then(() => {
+    alert("Email Sent!");
+  })
 }
 
 
